927b4267c8587676fae56926f672e0e1ccd390d0
Charles Salarzon <salarzoncharles016@gmail.com>
2026-07-13 19:15:10 +0800

    database injection fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ["firebase-admin"],
+  },
 };
 
 module.exports = nextConfig;
